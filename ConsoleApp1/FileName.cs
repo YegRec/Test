a141312bb7e1ba8a17551beb8e5e39b497537160
Yenery Rafael <yeneri@rgr@hotmail.com>
2025-02-18 01:54:29 -0800

    test Branch

--- ConsoleApp1/FileName.cs
+++ ConsoleApp1/FileName.cs
@@ -10,7 +10,8 @@ namespace ConsoleApp1
     {
         static  void Main(string[] args)
         {
-
+            Console.WriteLine("Git cambio");
+            Console.WriteLine("Chanchito feliz");
             Console.WriteLine("Hola mundo");
 
         }
