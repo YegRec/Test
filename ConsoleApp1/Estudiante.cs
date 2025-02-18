26d56df24b3184198fe37aa1f46d5b79cce8bc40
Yenery Rafael <yeneri@rgr@hotmail.com>
2025-02-18 03:20:38 -0800

    Primer avance en clases de estudiantes

--- ConsoleApp1/Estudiante.cs
+++ ConsoleApp1/Estudiante.cs
@@ -3,10 +3,61 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static ConsoleApp1.program;
 
 namespace ConsoleApp1
 {
     internal class Estudiante
     {
+        public string Nombre { get; private set; }
+        public string Apellido { get; private set; }
+        public int Edad {  get; private set; }
+        public double Promedio { get; private set; }
+        public string Matricula { get; private set; }
+
+
+        public Estudiante(string nombre, string apellido, int edad, double promedio)
+        {
+            Nombre = nombre;
+            Apellido = apellido;
+            Edad = edad;
+            Promedio = promedio;
+            Matricula = GenerarMatricula(nombre, edad, apellido);
+        }
+
+        public void ActualizarPromedio(double promedio)
+        {
+            Promedio = promedio;
+        }
+
+        //Generaremos una matricula usando el nombre, edad, apellido y el dia en que
+        //el estudiante es registrado.
+        private string GenerarMatricula(string nombre, int edad, string apellido)
+        {
+            return "EST-" + nombre.Substring(0,1) + edad.ToString().Substring(1) + DateTime.Now.Day + apellido.Substring(0,1);
+        }
+
     }
+
+    internal class GrupoEstudiantes<T>
+    {
+        public List<T> ListaEstudiantes { get; private set; } = new List<T>();
+
+        public event AgregarEstudianteEventHandler<T> EventoAgregarEstudiantes;
+
+        public void AgregarEstudiante(T estudiante)
+        {
+            EventoAgregarEstudiantes?.Invoke(estudiante);
+            ListaEstudiantes.Add(estudiante);
+        }
+
+        public void EliminarEstudiante(string nombre, string apellido)
+        {
+
+
+        }
+
+
+    }
+
 }
---
+++ ConsoleApp1/FileName.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class program
+    {
+        public delegate void AgregarEstudianteEventHandler<T>(T estudiante);
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"{DateTime.Now.Day}");
+
+
+        }
+
+
+
+
+    }
+}
