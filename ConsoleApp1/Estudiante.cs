bcb07cef2c79599f3b924188337772016c540e1e
Yenery Rafael <yeneri@rgr@hotmail.com>
2025-02-18 15:09:04 -0800

    Metodos para trabajar con estudiantes

--- ConsoleApp1/Estudiante.cs
+++ ConsoleApp1/Estudiante.cs
@@ -34,12 +34,12 @@ namespace ConsoleApp1
         //el estudiante es registrado.
         private string GenerarMatricula(string nombre, int edad, string apellido)
         {
-            return "EST-" + nombre.Substring(0,1) + edad.ToString().Substring(1) + DateTime.Now.Day + apellido.Substring(0,1);
+            return "EST-" + nombre.Substring(0,1) + edad.ToString().Substring(1) + DateTime.Now.Day + DateTime.Now.Year.ToString().Substring(0,2) + apellido.Substring(0,1);
         }
 
     }
 
-    internal class GrupoEstudiantes<T>
+    internal class GrupoEstudiantes<T> where T : Estudiante
     {
         public List<T> ListaEstudiantes { get; private set; } = new List<T>();
 
@@ -51,10 +51,28 @@ namespace ConsoleApp1
             ListaEstudiantes.Add(estudiante);
         }
 
-        public void EliminarEstudiante(string nombre, string apellido)
+        public void EliminarEstudiante(string matricula)
         {
+            T estudiante = ListaEstudiantes.FirstOrDefault(x => x.Matricula == matricula);
 
+            if (estudiante != null)
+            {
+                ListaEstudiantes.Remove(estudiante);
+            }
+            else
+            {
+                throw new InvalidOperationException($"El estudiante {matricula} no existe");
+            }
+        }
 
+        public double ObtenerPromedioGeneral()
+        {
+            if (ListaEstudiantes.Count == 0)
+            {
+                throw new InvalidOperationException($"No existen estudiantes");
+            }
+
+            return ListaEstudiantes.Sum(x => x.Promedio);
         }
 
 
