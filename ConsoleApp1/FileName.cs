81b0a1d82c4e5d4157cab4be7b32948afa745d6b
Yenery Rafael <yeneri@rgr@hotmail.com>
2025-02-18 21:34:47 -0800

    Metodo agregado y vinculado al evento

--- ConsoleApp1/FileName.cs
+++ ConsoleApp1/FileName.cs
@@ -9,9 +9,20 @@ namespace ConsoleApp1
     internal class program
     {
         public delegate void EstudianteEventHandler<T>(T estudiante);
+
+        public static void MetodoAgEstudiante(Estudiante estudiante)
+        {
+            Console.WriteLine($"\nSe ha agregado un nuevo estudiante:\n" +
+                $"Nombre: {estudiante.Nombre} {estudiante.Apellido}\n" +
+                $"Edad: {estudiante.Edad}\n" +
+                $"Promedio: {estudiante.Promedio}\n" +
+                $"Matricula: {estudiante.Matricula}");
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine($"{DateTime.Now.Day}");
+            GrupoEstudiantes<Estudiante> Grupo1 = new GrupoEstudiantes<Estudiante>();
+            Grupo1.EventoAgregarEstudiantes += MetodoAgEstudiante;
+
 
 
         }
