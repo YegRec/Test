a26095dc24c053de43c95eadffc999e06e969e1c
Yenery Rafael <yeneri@rgr@hotmail.com>
2025-02-18 22:55:24 -0800

    Agregado verificaciones

---
+++ ConsoleApp1/Verificaciones.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.Json;
+using System.Text;
+using System.Threading.Tasks;
+using static ConsoleApp1.program;
+using System.Text.Json.Serialization.Metadata;
+using System.Text.Json.Serialization;
+
+
+namespace ConsoleApp1
+{
+
+    internal class Validaciones
+    {
+
+
+
+
+
+    }
+
+
+}
